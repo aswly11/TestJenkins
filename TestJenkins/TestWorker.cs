13d708ddf3209a0b52d999184c78dbf819dc2dae
aswly <aswly98@gmail.com>
2023-06-07 09:13:53 +0300

    add hosted service

---
+++ TestJenkins/TestWorker.cs
@@ -0,0 +1,20 @@
+﻿namespace TestJenkins
+{
+    public class TestWorker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+           
+            Timer timer = new Timer(testTimer, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+            return  Task.CompletedTask;
+        }
+
+        private void testTimer(object? state)
+        {
+            Console.WriteLine("Ahmed Test"+DateTime.Now.ToString());
+        }
+
+     
+    }
+}
